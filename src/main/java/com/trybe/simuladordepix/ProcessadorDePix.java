5afbc7d049cd6f91282adc4684ae9df7f8617323
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-02-01 18:32:13 -0300

    Criação da lógica de negócio da operação. Implementação das validações e envio ao servidor na nuvem, interpretando a resposta recebida.

--- src/main/java/com/trybe/simuladordepix/ProcessadorDePix.java
+++ src/main/java/com/trybe/simuladordepix/ProcessadorDePix.java
@@ -2,6 +2,7 @@ package com.trybe.simuladordepix;
 
 import java.io.IOException;
 
+/**Classe ProcessadorDePix. */
 public class ProcessadorDePix {
 
   private final Servidor servidor;
@@ -21,7 +22,30 @@ public class ProcessadorDePix {
    * @throws IOException Caso aconteça algum problema relacionado à comunicação
    *                     entre o aplicativo e o servidor na nuvem.
    */
-  public void executarPix(int valor, String chave) throws ErroDePix, IOException {
-    // TODO: Implementar.
+  public void executarPix(int valor, String chave) throws ErroDePix, IOException,
+        ErroValorNaoPositivo, ErroChaveEmBranco, ErroSaldoInsuficiente, 
+        ErroChaveNaoEncontrada, ErroInterno {
+    try {
+      if (valor <= 0) {
+        throw new ErroValorNaoPositivo();
+      } else if (chave.isBlank()) {
+        throw new ErroChaveEmBranco();
+      }
+
+      servidor.abrirConexao();
+
+      switch (servidor.abrirConexao().enviarPix(valor, chave)) {
+        case CodigosDeRetorno.SUCESSO:
+          break;
+        case CodigosDeRetorno.SALDO_INSUFICIENTE:
+          throw new ErroSaldoInsuficiente();
+        case CodigosDeRetorno.CHAVE_PIX_NAO_ENCONTRADA:
+          throw new ErroChaveNaoEncontrada();
+        default:
+          throw new ErroInterno();
+      }
+    } finally {
+      servidor.abrirConexao().close();
+    }
   }
 }
